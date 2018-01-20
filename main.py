e4272fecd35a65d49f2dee641cebb1f87b9c0670
DongjunLee <humanbrain.djlee@gmail.com>
2018-01-20 18:57:21 +0900

    refactor(hb-base): add print_verbose, debug mode and slack_notification

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #-- coding: utf-8 -*-
 
 import argparse
+import atexit
 import logging
 
 from hbconfig import Config
 import tensorflow as tf
+from tensorflow.python import debug as tf_debug
 
 import data_loader
 from model import Conversation
 import hook
+import utils
 
 
 
@@ -29,20 +32,27 @@ def experiment_fn(run_config, params):
     train_input_fn, train_input_hook = data_loader.make_batch((train_X, train_y), batch_size=Config.model.batch_size)
     test_input_fn, test_input_hook = data_loader.make_batch((test_X, test_y), batch_size=Config.model.batch_size, scope="test")
 
+    train_hooks = [train_input_hook]
+    if Config.train.print_verbose:
+        train_hooks.append(hook.print_variables(
+            variables=['train/enc_0', 'train/dec_0', 'train/pred_0'],
+            rev_vocab=utils.get_rev_vocab(vocab),
+            every_n_iter=Config.train.check_hook_n_iter))
+    if Config.train.debug:
+        train_hooks.append(tf_debug.LocalCLIDebugHook())
+
+    eval_hooks = [test_input_hook]
+    if Config.train.debug:
+        eval_hooks.append(tf_debug.LocalCLIDebugHook())
+
     experiment = tf.contrib.learn.Experiment(
         estimator=estimator,
         train_input_fn=train_input_fn,
         eval_input_fn=test_input_fn,
         train_steps=Config.train.train_steps,
         min_eval_frequency=Config.train.min_eval_frequency,
-        train_monitors=[
-            train_input_hook,
-            hook.print_variables(
-                variables=['train/enc_0', 'train/dec_0', 'train/pred_0'],
-                vocab=vocab,
-                every_n_iter=Config.train.check_hook_n_iter)],
-        eval_hooks=[test_input_hook],
-        eval_delay_secs=0
+        train_monitors=train_hooks,
+        eval_hooks=eval_hooks,
     )
     return experiment
 
@@ -74,7 +84,15 @@ if __name__ == '__main__':
 
     tf.logging._logger.setLevel(logging.INFO)
 
+    # Print Config setting
     Config(args.config)
     print("Config: ", Config)
+    if Config.get("description", None):
+        print("Config Description")
+        for key, value in Config.description.items():
+            print(f" - {key}: {value}")
+
+    # After terminated Notification to Slack
+    atexit.register(utils.send_message_to_slack, config_name=args.config)
 
     main(args.mode)
